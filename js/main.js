98317ba9ba0605e869807a5705b7f6cb2c90f84f
Valentin Pundikov <nkbotcman@gmail.com>
2024-12-13 12:04:13 +0300

    del js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// JavaScript for handling form submission
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for your message!');
-    this.reset();
-});
